82c95c363a9f4d6bb63318070522979af1ef8817
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-11-07 21:06:36 +0900

    react setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React from "react";
+import * as React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <>
+      <div className="App">hello</div>
+    </>
   );
 }
 
